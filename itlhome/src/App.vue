d20c6eb34fd852b68f1b6b1cd1a4ab97290dccff
Axel Facundo Duartes <afduartes@latam.stefanini.com>
2021-10-06 17:02:25 -0300

    Punto creacion itlHome

---
+++ itlhome/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view/>
+</template>
+
+<style>
+#app {
+     @import "@/sass/utility/anims.scss";
+    @import "@/sass/utility/fonts.scss";
+    @import "@/sass/utility/normalize.scss";
+    @import "@/sass/utility/bootstrap.scss";
+    @import "@/sass/utility/utility.scss";
+
+    @import "@/sass/typography/icons.scss";
+    @import "@/sass/typography/typography.scss";
+
+    @import '~vue-datetime/dist/vue-datetime.css';
+
+    @import "@/sass/components/button.scss";
+    @import "@/sass/components/datetime.scss";
+    @import "@/sass/components/form.scss";
+    @import "@/sass/components/grid.scss";
+    @import "@/sass/components/layout.scss";
+    @import "@/sass/components/table.scss";
+    @import "@/sass/components/paginator.scss";
+
+    @import "@/sass/misc.scss";
+}
+</style>
