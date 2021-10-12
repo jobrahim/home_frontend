5a8208bcb1774452e25b6805a4a30d8053ad2c57
Ivan Leonel Corbo <ilcorbo@latam.stefanini.com>
2021-10-12 15:41:43 -0300

    implementacion Keycloak

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router/router'
 import Vue from 'vue'
 window.Vue = require('vue').default;
 import '@fortawesome/fontawesome-free/js/all'
+import * as Keycloak from 'keycloak-js'
 
 import VueTranslate from 'vue-translate-plugin';
 
@@ -12,9 +13,40 @@ import VueTranslate from 'vue-translate-plugin';
 // 	es: es
 // });
 
+let initOptions = {
+    url: process.env.VUE_APP_KEYCLOAK_URL,
+    realm: process.env.VUE_APP_KEYCLOAK_REALM,
+    clientId: process.env.VUE_APP_KEYCLOAK_CLIENT_ID,
+    client_secret: process.env.VUE_APP_KEYCLOAK_CLIENT_SECRET,
+    onLoad: 'login-required'
+}
+
+let keycloak = Keycloak(initOptions);
+
+function refreshToken() {
+    keycloak.updateToken(70).then((refreshed) => {
+        if (refreshed) {
+            localStorage.setItem("vue-token", keycloak.token);
+            localStorage.setItem("vue-refresh-token", keycloak.refreshToken);
+        }
+    }).catch(() => {
+        window.location.reload();
+    });
+}
+
+keycloak.onTokenExpired = function () {
+    refreshToken();
+}
 
 
-createApp(App).use(router).mount('#app')
+window.user = null;
+
+keycloak.init({ onLoad: initOptions.onLoad }).then((auth) => {
+
+    createApp(App).use(router).mount('#app')
+
+}).catch(() => {
+});
 
 
 
