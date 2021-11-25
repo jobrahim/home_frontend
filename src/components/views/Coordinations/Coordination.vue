a1330c969569161ce533a2da9f03448627fb19f1
Ivan Leonel Corbo <ilcorbo@latam.stefanini.com>
2021-11-25 09:07:13 -0300

    Fix iframe

--- src/components/views/Coordinations/Coordination.vue
+++ src/components/views/Coordinations/Coordination.vue
@@ -1,27 +1,25 @@
 <template>
-<!-- <loader  ></loader> -->
-  <iframe src="coordination" frameborder="0"   width="100%" height="1000" scrolling="no" ></iframe>
+  <!-- <loader  ></loader> -->
+  <iframe
+    :src="coordination"
+    frameborder="0"
+    width="100%"
+    height="1000"
+    scrolling="no"
+  ></iframe>
 </template>
 
-
-
-
-
 <script>
-
-import loader from "../../shared/LoaderComponent.vue"
-import {COORDINATION_URL} from '../../../utils/Constants'
+import loader from "../../shared/LoaderComponent.vue";
+import { COORDINATION_URL } from "../../../utils/Constants";
 export default {
-
-components:{
-  loader
-},
-data(){
-  return{
-coordination:COORDINATION_URL
-  }
-}
-
-}
+  components: {
+    loader,
+  },
+  data() {
+    return {
+      coordination: COORDINATION_URL,
+    };
+  },
+};
 </script>
-
